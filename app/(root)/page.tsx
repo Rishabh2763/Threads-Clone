b9db2801b18af49133545b2b9d6eb5c0a07a5074
Rishabh <rishabhlakhaiyar27@gmail.com>
2023-09-16 22:22:08 +0530

    Clerk Auth initialized

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import '../globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Threads</h1>
+    </main>
+  )
+}
