c00b172d1d1fcb205ca7786be418c1271a071261
Rishabh <rishabhlakhaiyar27@gmail.com>
2023-09-17 08:03:41 +0530

    conflicts handled

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,32 +1,16 @@
-<<<<<<< HEAD
-import {ClerkProvider} from "@clerk/nextjs";
-import {Inter} from "next/font/google";
-=======
+
+
 import {ClerkProvider} from '@clerk/nextjs';
 
 import {Inter} from 'next/font/google'
->>>>>>> dev2
+
 import '../globals.css';
 export const metadata={
   title: "Threads",
   description: "A Next.js 13 Meta Threads Application"
 }
 
-<<<<<<< HEAD
 
-const inter=Inter({subsets:["latin"]})
-
-export default function RootLayout({children}:{children:React.ReactNode}){
-  return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={`${inter.className} bg-dark-1`}>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
-  )
-=======
 const inter=Inter({subsets:["latin"]});
 
 export default function RootLayout({
@@ -41,5 +25,5 @@ export default function RootLayout({
       </html>
     </ClerkProvider>
   );
->>>>>>> dev2
+
 }
